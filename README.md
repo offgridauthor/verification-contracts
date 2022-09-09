4cb24b0f52de21b674146c6a269a9e0b4b628fd8
Stephen Webber <offgridauthor@gmail.com>
2022-09-08 21:39:27 -0500

    update scripts

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # demo-contracts
+
+Example repo to demonstrate bytecode verification review feature for contract upgrades in OpenZeppelin Defender.
+
+## Scripts
+
+- `npm deploy` - Deploy token contract, add to Defender Admin and verify bytecode
+- `npm upgrade` - Deploy token contract V2, verify bytecode and create Admin proposal for signers to review
+- `npm upgrade-admin` - Same as `npm upgrade`, using `admin-client` instead
+
+## Reference
+
+- [Bytecode Verification - Docs](https://docs.openzeppelin.com/defender/admin#bytecode-verification)
+- [Hardhat Defender - NPM](https://www.npmjs.com/package/@openzeppelin/hardhat-defender)
+- [Upgrades Repo](https://github.com/OpenZeppelin/openzeppelin-upgrades)
+- [Hardhat Upgrades Plugin Documentation](https://docs.openzeppelin.com/upgrades-plugins/1.x/api-hardhat-upgrades#defender-propose-upgrade)
+- [Defender Admin Client - NPM](https://www.npmjs.com/package/defender-admin-client)
---
+++ scripts/v2-propose-and-verify.js
@@ -0,0 +1,29 @@
+const { defender } = require('hardhat');
+require('dotenv').config();
+
+// deploy v2 implementation and propose upgrade, submitting bytecode for verification
+
+const PROXY_ADDRESS='0x65d6ed59533e5D89D15d4663cA6b209Bf2E8436a';
+const BUILD_ARTIFACT_URL='https://raw.githubusercontent.com/offgridauthor/verification-contracts/main/artifacts/build-info/b42f250d48743104c16e746045c66dea.json';
+
+async function main() {
+  const proposal = await defender.proposeUpgrade(PROXY_ADDRESS, 'VotingTokenV2',{ 
+    bytecodeVerificationReferenceUrl: BUILD_ARTIFACT_URL,
+    kind: 'uups',
+    description: `Upgrading to new version deployed at ${BUILD_ARTIFACT_URL}`,
+    multisig: '0x88BECCF8F957649c7701b670BFAA7318967f7412',
+    multisigType: 'Gnosis Safe', 
+  });
+// multisig options: 'Gnosis Safe' | 'Gnosis Multisig' | 'EOA';
+// Gnosis Safe is the latest offering. it is what's created if you use Defender to create your multisig. 
+// Gnosis multisig - a legacy offering of gnosis prior to the Safe
+// EOA - eg metamask, a single signer account
+
+  const verification = proposal.verificationResponse;
+  console.log(`Created new upgrade proposal at ${proposal.url} for artifact with digest ${verification?.providedSha256 ?? 'unknown'} (match ${verification.matchType})`);
+}
+
+if (require.main === module) {
+  main().then(() => process.exit(0))
+    .catch(error => { console.error(error); process.exit(1); });
+}
