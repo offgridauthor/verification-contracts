302ef6f7fc9aff4814f62bbd92633ab3699b5cde
Stephen Webber <offgridauthor@gmail.com>
2022-09-10 19:38:00 -0500

    add workflow

--- scripts/v2-propose-and-verify.js
+++ scripts/v2-propose-and-verify.js
@@ -1,17 +1,17 @@
 const { defender } = require('hardhat');
-require('dotenv').config();
 
 // deploy v2 implementation and propose upgrade, submitting bytecode for verification
+const owner = process.env.OWNER // '0x88BECCF8F957649c7701b670BFAA7318967f7412'
+const address= process.env.ADDRESS // '0x65d6ed59533e5D89D15d4663cA6b209Bf2E8436a'
+const url = process.env.WORKFLOW_URL // 'https://raw.githubusercontent.com/offgridauthor/verification-contracts/main/artifacts/build-info/b42f250d48743104c16e746045c66dea.json'
 
-const PROXY_ADDRESS='0x65d6ed59533e5D89D15d4663cA6b209Bf2E8436a';
-const BUILD_ARTIFACT_URL='https://raw.githubusercontent.com/offgridauthor/verification-contracts/main/artifacts/build-info/b42f250d48743104c16e746045c66dea.json';
 
 async function main() {
-  const proposal = await defender.proposeUpgrade(PROXY_ADDRESS, 'VotingTokenV2',{ 
-    bytecodeVerificationReferenceUrl: BUILD_ARTIFACT_URL,
+  const proposal = await defender.proposeUpgrade(address, 'VotingTokenV2',{ 
+    bytecodeVerificationReferenceUrl: url,
     kind: 'uups',
-    description: `Upgrading to new version deployed at ${BUILD_ARTIFACT_URL}`,
-    multisig: '0x88BECCF8F957649c7701b670BFAA7318967f7412',
+    description: `Upgrading to new version deployed at ${url}`,
+    multisig: owner,
     multisigType: 'Gnosis Safe', 
   });
 // multisig options: 'Gnosis Safe' | 'Gnosis Multisig' | 'EOA';
